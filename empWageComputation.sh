519b6876d2f29e155670d77696cefecf585611b9
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-09-29 22:09:37 +0530

    Added: Switch case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,6 +3,7 @@
 echo "Welcome to Employee Wage Computation  Program"
 
 isPresent=1
+isFullTime=2
 randomCheck=$(( 1 + RAMDOM % 2 ))
 if [ $isPresent -eq $randomCheck ]
 then
@@ -14,12 +15,35 @@ fi
 echo "Calculate Daily employee Full Time Wages"
 wagePerHour=20
 hoursPerDay=8
-
 wagePerDay=$(( 20 * 8 ))
-
 echo "Daily Employee wages= $wagePerDay"
 
+salary=0
+totalSalary=0
 echo "Calculate Employee Part time & Wages"
 partTimeHourPerday=4
 partTimeWage=$(( $wagePerHour * $partTimeHourPerday ))
 echo "Daily Employee Part time wages= $partTimeWage"
+
+
+read -p "Enter the number of working day =" numDay
+for (( day=1; day<=$numDay; day++ ))
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ))
+	case $employeeCheck in
+		$isFullTime)
+			empName=FullTimeEmp
+			empHrs=8
+		;;
+		$isPartTime)
+			empName=PartTimeEmp
+			empHrs=4
+		;;
+	esac
+
+	salary=$(( $empHrs * $WagePerHour ))
+	echo "Salary of $empName on the $day is $salary"
+	totalSalary=$(( $totalSalary + $salary ))
+done
+	
+echo "Total Salary = $totalSalary" 
