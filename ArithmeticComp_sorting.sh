e8c2b0d3353d866e3ddd47a3841f1f90e0f1330f
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-09-30 18:13:45 +0530

    Added: Compute result of 2nd Expr a*b+c

--- ArithmeticComp_sorting.sh
+++ ArithmeticComp_sorting.sh
@@ -3,5 +3,9 @@
 echo "Welcome in Arithmetic Computation and  Sorting"
 
 read -p "Enter the values of a b and c " a b c
+
 result=$((a+b*c))
 echo "Result of ((a+b*c)) is = $result"
+
+result=$((a*b+c))
+echo "Result of ((a*b+c)) is = $result"
