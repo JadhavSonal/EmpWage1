1d0a6dc0584f18d110badc49ab4acf71d603df52
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-09-30 21:00:59 +0530

    Added: Store result in Dictionary

--- ArithmeticComp_sorting.sh
+++ ArithmeticComp_sorting.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -x
 
+declare -A  Dictionary
 echo "Welcome in Arithmetic Computation and  Sorting"
 
 read -p "Enter the values of a b and c " a b c
 
-result=$((a+b*c))
+result1=$((a+b*c))
 echo "Result of ((a+b*c)) is = $result"
 
-result=$((a*b+c))
+result2=$((a*b+c))
 echo "Result of ((a*b+c)) is = $result"
 
-result=$((c+a/b))
+result3=$((c+a/b))
 echo "Result of ((c+a/b)) is = $result"
 
-result=$((a%b+c))
+result4=$((a%b+c))
 echo "Result of ((a%b+c)) is = $result"
+
+Dictionary[1]=$result1
+Dictionary[2]=$result2
+Dictionary[3]=$result3
+Dictionary[4]=$result4
+echo "Displaying all elements  present in disctionary:" ${Dictionary[@]}
