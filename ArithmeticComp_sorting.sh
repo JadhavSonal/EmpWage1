39b79263b3b89f9ddd17a4e2ea2953b46bb68b1c
Sonal Jadhav <jadhavsonal94@gmail.com>
2020-09-30 17:56:40 +0530

    Added: Three Input values a b & c

---
+++ ArithmeticComp_sorting.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+echo "Welcome in Arithmetic Computation and  Sorting"
+
+read -p "Enter the values of a b and c " a b c
+
--- ArithmeticComputationSort.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -x
-
-echo "Welcome in Arithmetic Computation Sorting"
